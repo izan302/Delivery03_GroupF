c866ccbfc60ee95cee70b6819b70526fd02a59d0
izan302 <izan302.yt@gmail.com>
2025-02-22 11:04:58 +0100

    Buttons and InventorySystem

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void LoadSceneByName(string _name) {
+        SceneManager.LoadScene(_name);
+    }
+
+    public void LoadNextScene() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+
+}
