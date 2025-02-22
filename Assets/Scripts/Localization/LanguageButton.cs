9f4cc24a44910e90bca6f7f828260d6b2f979e48
Vyktor <victureca3@gmail.com>
2025-02-22 13:21:35 +0100

    Localization

---
+++ Assets/Scripts/Localization/LanguageButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LanguageButton : MonoBehaviour, IPointerClickHandler
+{
+    public Language Language;
+
+    private Text _localizedText;
+
+    public void Start()
+    {
+        _localizedText = gameObject.GetComponentInChildren<Text>();
+        _localizedText.text = Language.ToString();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Language += 1;
+        if (Language > Language.Spanish) Language = Language.English;
+
+        Localizer.SetLanguage(Language);
+
+        // Make sure button displays current language text
+        _localizedText.text = Language.ToString();
+    }
+}
---
+++ Assets/Scripts/Localization/LocalizeText.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizeText : MonoBehaviour
+{
+    public string TextKey;
+    private Text _textValue;
+
+    void Start()
+    {
+        _textValue = GetComponent<Text>();
+        _textValue.text = Localizer.GetText(TextKey);
+    }
+
+    private void OnEnable()
+    {
+        Localizer.OnLanguageChange += ChangeLanguage;
+    }
+
+    private void OnDisable()
+    {
+        Localizer.OnLanguageChange -= ChangeLanguage;
+    }
+
+    private void ChangeLanguage()
+    {
+        _textValue.text = Localizer.GetText(TextKey);
+    }
+
+
+}
