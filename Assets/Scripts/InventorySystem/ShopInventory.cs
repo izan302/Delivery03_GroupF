c3d46ae55e63b7d62b29e8303e996948149b0cd4
Isurpri <hamsisaac@gmail.com>
2025-02-25 10:46:18 +0100

    Title/Ending/Fixed Start Selling

--- Assets/Scripts/InventorySystem/ShopInventory.cs
+++ Assets/Scripts/InventorySystem/ShopInventory.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class ShopInventory : MonoBehaviour
 {
     public Inventory Inventory;   
+    public void PickUp(ICanBePicked item)
+    {
+        // Add picked up item to shop inventory
+        Inventory.AddItem(item.GetItem());
+    }    
 }
